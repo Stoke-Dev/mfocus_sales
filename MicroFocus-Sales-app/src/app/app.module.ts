3311587506aef061099162c9861305cf8ad75f42
Brandyn Burbank <brandyn@brandynburbank.com>
2018-02-12 17:16:44 -0700

    Basic side bar and main content area layout setup

--- MicroFocus-Sales-app/src/app/app.module.ts
+++ MicroFocus-Sales-app/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SidebarComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
