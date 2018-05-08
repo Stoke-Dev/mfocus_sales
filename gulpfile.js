035b18aef3eb932e9bbc5339f6ba439ef12047d7
Brandyn Burbank <brandyn@brandynburbank.com>
2018-05-08 12:02:49 -0600

    updates to gulp workflow (prefixer and minify)

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,17 @@
 'use strict';
  
-var gulp = require('gulp'),
+let gulp = require('gulp'),
     sass = require('gulp-sass'),
     browserSync = require('browser-sync').create(),
-    closureCompiler = require('gulp-closure-compiler');
+    closureCompiler = require('gulp-closure-compiler'),
+    cleanCSS = require('gulp-clean-css'),
+    autoprefixer = require('gulp-autoprefixer');
  
 gulp.task('sass', function () {
   return gulp.src('./app/scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(cleanCSS())
     .pipe(gulp.dest('./app/css'))
     .pipe(browserSync.stream());
 });
