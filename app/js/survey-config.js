cb718fbb3b4073d00db4cd81b9487336cd7d623b
Brandyn Burbank <brandyn@brandynburbank.com>
2018-06-11 19:56:38 -0600

    survey layout changes

--- app/js/survey-config.js
+++ app/js/survey-config.js
@@ -10,35 +10,46 @@ $(document).ready(function(){
 
         survey = new Survey.Model(surveyJSON);
 
-        $("#surveyElement").Survey({
-            model:survey,
-            onComplete:sendDataToServer,
-            onUpdatePanelCssClasses:(a,b)=>{
-                var el_id = "#"+b.panel.id;
+        //survey.completedHtml = "Generating Report...\n[coming soon]",
+        survey.onComplete.add(sendDataToServer);
+        survey.questionTitleLocation = "left";
+        survey.onUpdatePanelCssClasses.add((a,b)=>{
+            let el_id = "#"+b.panel.id;
 
-                if (b.panel.name != "client_data") {
-                    b.panel.state = "collapsed";
-                }
-                
-                if (b.panel.parent && b.panel.parent.isPanel) {
-                    $(el_id).addClass("mf-sub-panel");
-                } else if (b.panel.isPanel) {
-                    $(el_id).addClass("mf-panel");
-                    // console.log(b);
-                }
-                
-            },
-            onUpdateQuestionCssClasses:(a,b)=>{
-                var el_id = "#"+b.question.id;
-                console.log(b);
+            b.cssClasses.row = "mf-survey-row";
 
-                $(el_id).addClass("mf-question");
+            if (b.panel.name != "client_data") {
+                b.panel.state = "collapsed";
             }
+            
+            if (b.panel.name == "client_data") {
+                b.cssClasses.panel.container = "sv_p_container first mf-panel";
+            } else if (b.panel.name == "ctes") {
+                b.cssClasses.panel.container = "sv_p_container last mf-panel";
+            } else if (b.panel.parent && b.panel.parent.isPanel) {
+                b.cssClasses.panel.container = "sv_p_container mf-sub-panel";
+            } else if (b.panel.isPanel) {
+                b.cssClasses.panel.container = "sv_p_container mf-panel";
+            }
+            
         });
+
+        survey.onUpdateQuestionCssClasses.add((a,b)=>{
+            b.cssClasses.mainRoot = "sv_qstn mf-question";
+            b.cssClasses.title = "mf-question-title";
+            b.cssClasses.error.root = "alert alert-danger mf-error";
+            b.cssClasses.error.icon = "fas fa-exclamation-triangle";
+            b.question.requiredErrorText = "This information is required."
+
+            console.log(b)
+
+        });
+
+        $("#surveyElement").Survey({model:survey});
 
         console.log("JSON LOADED: SUCCESS");
     })
-    .fail(function(jqxhr, textStatus, err) {
+    .fail((jqxhr, textStatus, err)=>{
         $("#surveyElement").html("ERROR: Form framework could not be loaded");
         console.log("JSON ERROR: ", err);
     });
