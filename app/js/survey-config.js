6080203f6e207f72450560c4c78059d59da30e62
Brandyn Burbank <brandyn@brandynburbank.com>
2018-06-15 14:20:40 -0600

    minor functional, and style changes

--- app/js/survey-config.js
+++ app/js/survey-config.js
@@ -10,12 +10,10 @@ $(document).ready(function(){
 
         survey = new Survey.Model(surveyJSON);
 
-        //survey.completedHtml = "Generating Report...\n[coming soon]",
+        survey.completedHtml = "<div style='text-align:center; margin:11px;'><h3 style='margin-bottom:11px;'>Generating Report...</h3><em>[coming soon]</em><br><img style='width: 70%; display:inline-block;' src='img/Micro_Focus_SalesROI_Results_v1.png' /></div>",
         survey.onComplete.add(sendDataToServer);
         survey.questionTitleLocation = "left";
         survey.onUpdatePanelCssClasses.add((a,b)=>{
-            let el_id = "#"+b.panel.id;
-
             b.cssClasses.row = "mf-survey-row";
 
             if (b.panel.name != "client_data" && b.panel.name != "data_panel") {
@@ -27,11 +25,10 @@ $(document).ready(function(){
             } else if (b.panel.name == "data_panel") {
                 b.cssClasses.panel.container = "sv_p_container last mf-panel";
             } else if (b.panel.parent && b.panel.parent.isPanel) {
-                b.cssClasses.panel.container = "sv_p_container mf-sub-panel";
+                b.cssClasses.panel.container = `sv_p_container mf-sub-panel p-${b.panel.name}`;
             } else if (b.panel.isPanel) {
                 b.cssClasses.panel.container = "sv_p_container mf-panel";
-            }
-            
+            }         
         });
 
         survey.onUpdateQuestionCssClasses.add((a,b)=>{
@@ -94,16 +91,29 @@ $(document).ready(function(){
 
         $("#mf_quick_directory").html(()=>{
             let markup = "";
-
             survey.getAllPanels().map((a)=>{
-                if (a.parent.isPanel) {
-                   markup += `<ul><li>${a.title}</li></ul>`;
+                if (a.parent.name == "data_panel") {
+                   markup += `<ul><li><strong><a data-quickpick href='#${a.id}'>${a.title}</a></strong></li></ul>`;
+                } else if (a.parent.name == "fte" || a.parent.name == "ctes" || a.parent.name == "cust") {
+                    markup += `<ul><li><a data-quickpick href='#${a.id}'>- ${a.title}</a></li></ul>`;
                 } else {
-                   markup += `<li>${a.title}</li>`;
+                   markup += `<li><a data-quickpick href='#${a.id}'>${a.title}</a></li>`;
                 }
             });
-
             return markup;
+        }).find("[data-quickpick]").each((i,e)=>{
+            $(e).click(({target})=>{
+                survey.page
+                let p = survey.getAllPanels().find((p)=>{
+                    return (`#${p.id}` == $(target).attr("href")); 
+                });
+                if (p.name == "data_panel" || p.name == "client_data") return;
+                p.expand();
+                while (p.parent.isPanel && p.parent.name != "data_panel") {
+                    p.parent.expand();
+                    p = p.parent;
+                }
+            });
         });
 
         console.log("JSON LOADED: SUCCESS");
@@ -119,4 +129,7 @@ $(document).ready(function(){
 function sendDataToServer(survey) {
     var resultAsString = JSON.stringify(survey.data);
     alert(resultAsString); //send Ajax request to your web server.
+    $("main.mf-maincontent > h1").html("Download Client Report");
+
+    $("#mf_quick_directory").html("");
 }
