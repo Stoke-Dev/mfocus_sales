d8dda818841cc9908bec495ea07d5020a9aff1b0
Brandyn Burbank <brandyn@brandynburbank.com>
2018-06-20 14:42:36 -0600

    new dependancies

--- gulpfile.js
+++ gulpfile.js
@@ -4,29 +4,71 @@ let gulp = require('gulp'),
     sass = require('gulp-sass'),
     browserSync = require('browser-sync').create(),
     cleanCSS = require('gulp-clean-css'),
-    autoprefixer = require('gulp-autoprefixer');
+    autoprefixer = require('gulp-autoprefixer'),
+    babel = require('gulp-babel'),
+    sourcemaps = require('gulp-sourcemaps'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify');
  
 gulp.task('sass', function () {
+    console.log("Running SASS Compiler + Minifying")
     return gulp.src('./app/scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer())
     .pipe(cleanCSS())
-    .pipe(gulp.dest('./app/css'))
+    .pipe(gulp.dest('dist/css'))
     .pipe(browserSync.stream());
 });
 
-gulp.task('serve', ['sass'], function() {
+gulp.task('babel', function () {
+    console.log("Running Babel Transpiler + Minifying")
+    return gulp.src("app/**/*.js")
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+        presets: ['env']
+    }))
+    .pipe(concat('app.js'))
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist/js'))
+});
+
+gulp.task('build', ['sass', 'babel'])
+
+gulp.task('flush-dist', function () {
+    return gulp.src('dist', {read: false})
+        .pipe(clean());
+});
+
+gulp.task('move-static', function() {
+    let filesToMove = [
+        'fonts/**/*.*',
+        'img/**/*.*',
+        'data/**/*.*',
+        'index.html',
+        'favicon.ico'
+    ];
+
+    // the base option sets the relative root for the set of files,
+    // preserving the folder structure
+    return gulp.src(filesToMove, { base: './app/' })
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('rebuild-static', ['flush-dist', 'move-static'])
+
+gulp.task('serve', ['build'], function() {
 
     browserSync.init({
         server: {
-            baseDir: "./app/"
+            baseDir: "./dist/"
         }
     });
 
     gulp.watch('./app/scss/**/*.scss', ['sass']);
     gulp.watch("app/**/*.html").on('change', browserSync.reload);
     gulp.watch("app/**/*.json").on('change', browserSync.reload);
-    gulp.watch("app/**/*.js").on('change', browserSync.reload);
+    gulp.watch("app/**/*.js", ['babel']).on('change', browserSync.reload);
 
 });
 
